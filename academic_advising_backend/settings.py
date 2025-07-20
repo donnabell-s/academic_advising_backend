1a46de39293ba7ea8fa169164cec202d79e5b947
donnabell-s <donnasembrano03@gmail.com>
2025-07-20 13:50:03 +0800

    db setup

--- academic_advising_backend/settings.py
+++ academic_advising_backend/settings.py
@@ -99,8 +99,12 @@ WSGI_APPLICATION = 'academic_advising_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'student_clustering',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
