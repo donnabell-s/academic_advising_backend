c2e857d9e215a06e77cdf107e633f5e3e3157fba
donnabell-s <donnasembrano03@gmail.com>
2025-07-28 14:03:38 +0800

    working advisor crud

--- clustering/serializers.py
+++ clustering/serializers.py
@@ -82,23 +82,3 @@ class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
         fields = ['id', 'student_id', 'name', 'program_and_grade', 'cluster', 'cluster_name']
-
-class AdvisorSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Advisor
-        fields = '__all__'
-
-class ClusterSerializer(serializers.ModelSerializer):
-    advisor_name = serializers.CharField(source='advisor.name', read_only=True)
-    student_count = serializers.IntegerField(read_only=True)
-
-    class Meta:
-        model = Cluster
-        fields = ['id', 'cluster_id', 'name', 'description', 'advisor', 'advisor_name', 'student_count']
-
-class StudentSerializer(serializers.ModelSerializer):
-    cluster_name = serializers.CharField(source='cluster.name', read_only=True)
-
-    class Meta:
-        model = Student
-        fields = ['id', 'student_id', 'name', 'program_and_grade', 'cluster', 'cluster_name']
